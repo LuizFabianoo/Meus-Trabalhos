fa96e9603faf3edd8ef0147ac3c50d34cf1fd8ee
LuizFabianoo <124620008+LuizFabianoo@users.noreply.github.com>
2023-02-06 17:33:01 -0300

    Add files via upload

---
+++ .github/workflows/script.js
@@ -0,0 +1,23 @@
+function estação() { 
+    let mês = prompt('Digite o mês em extenso (ex: Setembro)')
+    let saída = document.querySelector('section#saida')
+    let estação
+    switch (mês.toUpperCase()) { 
+        case 'JANEIRO': case 'FEVEREIRO': case 'MARÇO': 
+            estação = 'INVERNO'
+            break 
+        case 'ABRIL': case 'MAIO': case 'JUNHO':
+            estação = 'PRIMAVERA'
+            break
+        case 'JULHO': case 'AGOSTO': case 'SETEMBRO':
+            estação = 'VERÃO'
+            break
+        case 'OUTUBRO': case 'NOVEMBRO': case 'DEZEMBRO':
+            estação = 'OUTONO'
+            break
+        default:
+            estação = 'INDEFINIDA'
+            break
+    }
+    saída.innerHTML = `<p>No mês de <mark>${mês}</mark>, estamos na estação <mark><strong>${estação}</strong></mark>.</p>`
+}
